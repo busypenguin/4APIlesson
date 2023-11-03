0a7f548fae259655aeda12e3168127c46b71f632
busypenguin <112860511+busypenguin@users.noreply.github.com>
2023-11-03 12:15:41 +0300

    publish photo to Telegram channel.

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,9 @@ from additional_settings import telegram_bot_token
 bot = telegram.Bot(token=telegram_bot_token)
 
 
-bot.send_message(chat_id='@The_best_pictures_of_space', text="I did it!")
+# bot.send_message(chat_id='@The_best_pictures_of_space', text="I did it!")
 
+
+
+bot.send_photo('@The_best_pictures_of_space', photo=open('images/KsiuGQL4_o.png', 'rb'))
 
