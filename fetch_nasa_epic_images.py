56536cce899ddb776b2f315e68ab0f744f2bb9f3
busypenguin <112860511+busypenguin@users.noreply.github.com>
2023-11-09 17:34:56 +0300

    Отредактирован fetch_nasa_epic_images.py

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from environs import Env
 
 
-def fetch_nasa_epic_launch():
+def fetch_nasa_epic_launch(nasa_api_key):
     payload = {'api_key': nasa_api_key}
     response = requests.get('https://api.nasa.gov/EPIC/api/natural', params=payload)
     response.raise_for_status()
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     env.read_env()
     nasa_api_key = env.str('NASA_API_KEY')
     Path("images").mkdir(parents=True, exist_ok=True)
-    fetch_nasa_epic_launch()
+    fetch_nasa_epic_launch(nasa_api_key)
