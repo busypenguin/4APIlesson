d2884fc881c1c95ad3d7adf955b0d725c4bee7b1
busypenguin <112860511+busypenguin@users.noreply.github.com>
2023-11-09 17:35:22 +0300

    Отредактирован fetch_nasa_images.py

--- fetch_nasa_images.py
+++ fetch_nasa_images.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from environs import Env
 
 
-def fetch_nasa_last_launch():
+def fetch_nasa_last_launch(nasa_api_key):
     images_number = 30
     payload = {'api_key': nasa_api_key, 'count': images_number}
     response = requests.get('https://api.nasa.gov/planetary/apod', params=payload)
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     env.read_env()
     nasa_api_key = env.str('NASA_API_KEY')
     Path("images").mkdir(parents=True, exist_ok=True)
-    fetch_nasa_last_launch()
+    fetch_nasa_last_launch(nasa_api_key)
